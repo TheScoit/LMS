d54e0d2e7ed90d8c190d417e47f171041315d1f5
TheScoit <fs301t@gmail.com>
2026-03-30 01:12:54 +0530

    "More updates"

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,23 @@
-import { auth } from "@/lib/auth" // path to your auth file
-import { toNextJsHandler} from "better-auth/next-js"
+import { auth } from "@/lib/auth";
 
-export const {POST , GET} = toNextJsHandler(auth);
+import arcjet ,{  ArcjetDecision, BotOptions, EmailOptions, ProtectSignupOptions, shield, SlidingWindowRateLimitOptions} from "@arcjet/next"
+
+import { toNextJsHandler } from "better-auth/next-js"
+import { NextRequest } from "next/server";
+
+const emailOptions = {
+    mode: "LIVE",
+    block: ["DISPOSABLE","INVALID","NO_MX_RECORDS"],
+} satisfies EmailOptions;
+
+// bot Protection
+const botOptions = {
+    mode: "LIVE",
+    allow: [],
+} satisfies BotOptions;
+
+const rateLimitOptions = {
+    mode: "LIVE",
+    interval: "2m",
+    max: 5,
+} satisfies SlidingWindowRateLimitOptions<[]>;
